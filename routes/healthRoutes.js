bf39bac96b2c772b2b14bb2ef0d8c218dafed8e2
Tuey <164839385+WinterJet2021@users.noreply.github.com>
2025-06-26 19:11:05 +0700

    Delete routes directory

--- routes/healthRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getHealthStatus } from '../controllers/healthController.js';
-
-const router = express.Router();
-
-// ✅ Health check
-router.get('/', getHealthStatus);
-
-export default router;
